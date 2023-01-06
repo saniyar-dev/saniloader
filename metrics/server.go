dba2153231c0553f5be7ac39fc3f2c0a100f576d
saniyar <saniyar.dev@gmail.com>
2023-01-06 22:15:10 +0330

    add: numberOfRequest and handlers metric

--- metrics/server.go
+++ metrics/server.go
@@ -1,12 +1,26 @@
 package metrics
 
 import (
-	"net/http"
+	"encoding/json"
 	"log"
-)
+	"net/http"
+	"strings"
+) 
 
 func metricsHttpHandler(w http.ResponseWriter, r *http.Request) {
-	
+	name := strings.Trim(r.URL.Path, "/")
+	metrics, err := getMetrics(name)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	res, err := json.Marshal(metrics)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.Write(res)
 }
 
 func runMetricsServer() {
